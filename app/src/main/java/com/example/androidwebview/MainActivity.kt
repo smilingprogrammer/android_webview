5a92b5589f8200668a8c52bf6ea6fb77a23ee343
smilingprogrammer <abdulsoburoyewale@gmail.com>
2021-11-02 16:54:13 +0100

    Android WebView completed

--- app/src/main/java/com/example/androidwebview/MainActivity.kt
+++ app/src/main/java/com/example/androidwebview/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.androidwebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = object : MyWebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                Toast.makeText(this@MainActivity, "Page Loaded", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                super.onReceivedError(view, request, error)
+                Toast.makeText(this@MainActivity, "Error occurred", Toast.LENGTH_SHORT).show()
+            }
+        }
+        webView.loadUrl("https://google.com")
+    }
+
+    override fun onBackPressed() {
+        val canGoBack = webView.canGoBack()
+        if (canGoBack) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    open inner class MyWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
+            view?.loadUrl(url)
+            return true
+        }
     }
 }

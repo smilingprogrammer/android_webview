e6891a2ab5ffc452b224cd71dd17a2bd9a3e2208
smilingprogrammer <66224527+smilingprogrammer@users.noreply.github.com>
2021-11-22 23:18:54 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/androidwebview/MainActivity.kt
+++ app/src/main/java/com/example/androidwebview/MainActivity.kt
@@ -8,6 +8,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 
+// Using Youtube API
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
 
